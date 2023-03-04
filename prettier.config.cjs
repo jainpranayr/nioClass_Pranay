a239684f4d85399ef599f84ac53dd3c99595c194
Pranay Jain <jpranay688@gmail.com>
2023-03-04 21:24:54 +0530

    🎉 initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: [require('prettier-plugin-tailwindcss')],
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'avoid',
+}
